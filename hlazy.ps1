4c73b393653aad7541882549fefb10146a2c8eb7
ROMAN MOONSHI <146112846+mdromanmoonshi@users.noreply.github.com>
2025-05-01 19:40:11 +0600

    Update hlazy.ps1

--- hlazy.ps1
+++ hlazy.ps1
@@ -1,9 +1,19 @@
 # Enable TLS 1.2 support
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-# Set download URL and destination path
+# Set download URL
 $DownloadURL = 'https://raw.githubusercontent.com/mdromanmoonshi/havij/main/havij.bat'
-$FilePath = "$env:TEMP\havij.bat"
+
+# Set Desktop path and create 'Arena' folder if it doesn't exist
+$DesktopPath = [Environment]::GetFolderPath('Desktop')
+$FolderPath = Join-Path $DesktopPath 'Arena'
+
+if (-not (Test-Path $FolderPath)) {
+    New-Item -Path $FolderPath -ItemType Directory | Out-Null
+}
+
+# Set destination file path
+$FilePath = Join-Path $FolderPath 'havij.bat'
 
 # Attempt to download the file
 try {
